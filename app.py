fc6ad6626bf753cb34b8211a8b0985c05de8f2cc
Kyle Mulka <repalviglator@yahoo.com>
2014-08-25 01:00:12 -0400

    modifying filtering parameters

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from urllib.parse import urlencode
+import logging
 
 import tornado.ioloop
 import tornado.web
@@ -13,14 +15,28 @@ max_lat = 42.3
 max_lng = -83.7
 
 def tweetstream_callback(tweet):
+    if tweet is None:
+        logging.error('tweet is none')
+        return
+
     if 'retweeted_by' in tweet:
         return
 
-    coord = tweet['coordinates']['coordinates']
-    lng = coord[0]
-    lat = coord[1]
-    coord_in_bounds = lat < min_lat or lat > max_lat or lng < min_lng or lng > max_lng
-    if coord_in_bounds and tweet['place']['name'] != 'Ann Arbor':
+    if tweet['coordinates'] is not None:
+        coord = tweet['coordinates']['coordinates']
+        lng = coord[0]
+        lat = coord[1]
+        coord_in_bounds = lat > min_lat and lat < max_lat and lng > min_lng and lng < max_lng
+    else:
+        coord_in_bounds = False
+
+    if tweet['place'] is not None:
+        place_name = tweet['place']['name']
+    else:
+        place_name = None
+
+    text = tweet['text'].lower()
+    if not coord_in_bounds and place_name != 'Ann Arbor' and '#annarbor' not in text and 'ann arbor' not in text:
         return
 
     for client in clients:
@@ -33,11 +49,13 @@ tweetstream_config = {
     "twitter_access_token_secret": os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
 }
 
-query_string = "locations=%s,%s,%s,%s" % (min_lng, min_lat, max_lng, max_lat)
-# query_string = "track=%23twitter"
+params = {
+    'locations': "%s,%s,%s,%s" % (min_lng, min_lat, max_lng, max_lat),
+    'track': '#annarbor,ann arbor'
+}
 
 stream = tweetstream.TweetStream(tweetstream_config)
-stream.fetch("/1.1/statuses/filter.json?" + query_string, callback=tweetstream_callback)
+stream.fetch("/1.1/statuses/filter.json?" + urlencode(params), callback=tweetstream_callback)
 
 
 class MainHandler(tornado.web.RequestHandler):
