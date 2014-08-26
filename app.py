01cfc75310c2c17689627e01f370eef640419f8e
Kyle Mulka <repalviglator@yahoo.com>
2014-08-26 00:55:26 -0400

    switched to tracking #twitter

--- app.py
+++ app.py
@@ -9,34 +9,12 @@ import tweetstream
 
 clients = set()
 
-min_lat = 42.2
-min_lng = -83.8
-max_lat = 42.3
-max_lng = -83.7
-
 def tweetstream_callback(tweet):
     if tweet is None:
         logging.error('tweet is none')
         return
 
     if 'retweeted_by' in tweet:
-        return
-
-    if tweet['coordinates'] is not None:
-        coord = tweet['coordinates']['coordinates']
-        lng = coord[0]
-        lat = coord[1]
-        coord_in_bounds = lat > min_lat and lat < max_lat and lng > min_lng and lng < max_lng
-    else:
-        coord_in_bounds = False
-
-    if tweet['place'] is not None:
-        place_name = tweet['place']['name']
-    else:
-        place_name = None
-
-    text = tweet['text'].lower()
-    if not coord_in_bounds and place_name != 'Ann Arbor' and '#annarbor' not in text and 'ann arbor' not in text:
         return
 
     for client in clients:
@@ -50,8 +28,7 @@ tweetstream_config = {
 }
 
 params = {
-    'locations': "%s,%s,%s,%s" % (min_lng, min_lat, max_lng, max_lat),
-    'track': '#annarbor,ann arbor'
+    'track': '#twitter'
 }
 
 stream = tweetstream.TweetStream(tweetstream_config)
